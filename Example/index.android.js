e461be68b540eae8c85971aa2d779e22bd44236d
Brent Vatne <brentvatne@gmail.com>
2016-10-04 12:52:12 -0700

    Change to new sample app

--- Example/index.android.js
+++ Example/index.android.js
@@ -1,6 +1,36 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-import Example from './Example';
+import Camera from 'react-native-camera';
 
-AppRegistry.registerComponent('Example', () => Example);
+class CameraTesting extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          onBarCodeRead={this._handleBarCodeRead}
+          style={styles.preview} />
+      </View>
+    );
+  }
+
+  _handleBarCodeRead = (data) => {
+    console.log(data);
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  preview: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+AppRegistry.registerComponent('Example', () => CameraTesting);
